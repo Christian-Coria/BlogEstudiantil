34e107539a37c2912d6be042c7be687a890d215d
Christian-Coria <desbloqueoenlinea@gmail.com>
2022-09-26 13:54:57 -0300

    descomentando porsiones de codigo de app para el inegrador

--- integrador/admin.py
+++ integrador/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 from .models import Formato ,Interprete,Tema, Genero, Album,  Discografica
 
-# admin.site.register(Formato)
-# admin.site.register(Interprete)
-# admin.site.register(Genero)
-# admin.site.register(Tema)
-# admin.site.register(Discografica)
-# admin.site.register(Album)
+admin.site.register(Formato)
+admin.site.register(Interprete)
+admin.site.register(Genero)
+admin.site.register(Tema)
+admin.site.register(Discografica)
+admin.site.register(Album)
--- integrador/models.py
+++ integrador/models.py
@@ -23,21 +23,21 @@ class Genero(models.Model):
 class Discografica(models.Model):
     pass
 
-# class Album(models.Model):
+class Album(models.Model):
 
-#     nombre = models.CharField(max_length=255)
-#     interprete = models.ForeignKey(Interprete,null=True, on_delete=models.CASCADE)
-#     genero = models.ForeignKey(Genero,null=True, on_delete=models.CASCADE)
-#     cant_temas = models.CharField(max_length=40)
-#     discografica = models.ForeignKey(Discografica,null=True, on_delete=models.CASCADE)
-#     formato = models.ForeignKey(Formato,null=True, on_delete=models.CASCADE)
-#     fec_lanzamiento = models.CharField(max_length=40)
-#     precio = models.IntegerField()
-#     cantidad = models.IntegerField()
-#     imagen = models.ImageField(upload_to="caratula", null=True, blank=True)
+    nombre = models.CharField(max_length=255)
+    interprete = models.ForeignKey(Interprete,null=True, on_delete=models.CASCADE)
+    genero = models.ForeignKey(Genero,null=True, on_delete=models.CASCADE)
+    cant_temas = models.CharField(max_length=40)
+    discografica = models.ForeignKey(Discografica,null=True, on_delete=models.CASCADE)
+    formato = models.ForeignKey(Formato,null=True, on_delete=models.CASCADE)
+    fec_lanzamiento = models.CharField(max_length=40)
+    precio = models.IntegerField()
+    cantidad = models.IntegerField()
+    imagen = models.ImageField(upload_to="caratula", null=True, blank=True)
   
-#     def __str__(self):
-#        return f'{self.nombre}'
+    def __str__(self):
+       return f'{self.nombre}'
 
 
 class Tema(models.Model):
